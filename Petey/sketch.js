0058aaa9ce1c306c3d52e9552eb15647b19c8259
Nakshh <nakshhkohli04@gmail.com>
2022-09-17 18:28:41 -0400

    Co-authored-by: josep5673 <josep5673@users.noreply.github.com>

--- Petey/sketch.js
+++ Petey/sketch.js
@@ -1,45 +1,66 @@
 //initial number of cookies    
 var num = 0;
+var score = 1;
 
 window.onload = function () {
-       
-        var space = document.getElementById("space");
-        
-        space.innerHTML = name + "Pete's Paradise";
+        document.getElementById("space").innerHTML = "Pete's Paradise";
 }
 
 var cookie = document.getElementById("cookie");
 
 function cookieClick() { 
-    num += 1;
+    num = num + score;
 
     var numbers = document.getElementById("numbers");
     
     //upgrade level for printing
     var upgradeLevel = document.getElementById("upgradeLevel");
     
-    numbers.innerHTML = num;      
-    //automatic Granny upgrade to 2x
-    if(num >= 30 ){
-        num += 2;
-        upgradeLevel.innerHTML = "Granny Level";
-    }
+    numbers.innerHTML = num;        
+}
 
-    //automatic factory upgrade to 10x
-    if(num >= 500) {
-        num += 10;
-        upgradeLevel.innerHTML = "Factory Level";
+function tark() {
+    if(num >= 50){
+        num = num-50;
+        setInterval(cookieClick, 200);
     }
+}
 
-    //automatic plant upgrade to 30x
-    if(num >= 1000) {
-        num += 30;
-        upgradeLevel.innerHTML = "Plant Level";
+function ear() {
+    if(num >= 200){
+        num = num-200;
+        setInterval(cookieClick, 100);
     }
+}
 
-    //automatic super factory upgrade to 1000x
-    if(num >= 100000) {
-        num += 1000;
-        upgradeLevel.innerHTML = "Super-Plant Level";
+function harrison() {
+    if(num >= 500){
+        num = num-500;
+        setInterval(cookieClick, 20);
     }
+}
+
+function parker() {
+    if(num >= 1000){
+        num = num-1000;
+        setInterval(cookieClick, 10);
+    }
+}
+
+function hillenbrand() {
+    if(num >= 3000){
+        num = num-3000;
+        setInterval(cookieClick, 2);
+    }
+}
+
+function starShip() {
+    if(num >= 1){
+        num = num - 1;
+        score = 2; 
+    }
+}
+
+function hammer(){
+    if(num >= 5000)
 }
