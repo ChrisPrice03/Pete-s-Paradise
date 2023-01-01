cacf1d80296634dcd88c3a80d50f4df6fa92ad85
ChrisPrice03 <112359083+ChrisPrice03@users.noreply.github.com>
2023-01-01 13:25:31 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ var professorCost = 1000000;
 var randNum;
     //prestige results
 var prestigeResult = "";
-var inp = prompt("Please enter DD amount: "); // = 0;
+var inp = 0; // = prompt("Please enter DD amount: ");
 window.onload = function () {
         setInterval(updatenum, 10);
         setInterval(randomEvents, 30000); 
